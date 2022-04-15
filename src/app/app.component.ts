2a83ba01ee972509cf2e8cdad67533c82c40c21b
Abdullah <abdullah.abrahams@4-sure.net>
2022-04-15 19:23:11 +0200

    completed version 0.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Navigation,
+  Manipulation,
+  EffectCards,
+  EffectCoverflow,
+  SwiperOptions,
+} from 'swiper';
+
+SwiperCore.use([Navigation, Manipulation, EffectCards, EffectCoverflow]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'carousel';
+  @ViewChild('swiperRef', { static: false }) sliderRef!: SwiperComponent;
+
+  config: SwiperOptions = {
+    navigation: true,
+    loop: true,
+    centeredSlides: true,
+    mousewheel: true,
+    direction: 'horizontal',
+    effect: 'coverflow',
+    grabCursor: true,
+    slideToClickedSlide: true,
+    preventClicks: false,
+    preventClicksPropagation: false,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 16,
+      },
+    },
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 0,
+      modifier: 1,
+      slideShadows: true,
+    },
+  };
+  slides = [
+    { iconName: 'important_devices', title: 'Get a device' },
+    { iconName: 'phone', title: 'Add a phone-line' },
+    { iconName: 'update', title: 'Upgrade' },
+    { iconName: 'cell_wifi', title: 'Mobile Internet' },
+    { iconName: 'wifi', title: 'Home Internet' },
+  ];
+
+  cardClicked(index: number) {
+    const activeIndex = this.sliderRef?.swiperRef?.activeIndex ?? 0;
+
+    if (index === activeIndex || Math.abs(index - activeIndex) === 5) {
+      alert(`${this.slides?.[index]?.title} was selected!`);
+    } else {
+      this.sliderRef?.swiperRef?.slideToLoop(index);
+    }
+  }
+
+  startClicked(e: MouseEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+    alert(`Start Here clicked!`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { SwiperModule } from 'swiper/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
